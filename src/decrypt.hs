e50c37312a76f47a3275a92a23e647affb78bb8c
Desktop <kurosu.0122.playstation@gmail.com>
2023-12-10 23:49:09 +0900

    add a new file

---
+++ src/decrypt.hs
@@ -0,0 +1,21 @@
+import           System.Directory
+import           System.Process
+import System.IO
+
+type Password = String
+
+-- >>> isEncrypted "invailde/path/to/file.pdf"
+-- (False, Nothing)
+--
+
+isEncrypted :: FilePath -> IO (Bool, Maybe Handle)
+isEncrypted "" = return (False, Nothing)
+isEncrypted path = do
+    current <- doesPathExist path
+    exist <- doesFileExist path
+    if current && exist
+        then do
+            (_, Just hout, _, _) <- createProcess (proc "exiftool" ["-s", "-T", "-Encrypted", path]) { std_out = CreatePipe }
+            out <- hGetContents hout
+            return $ (length out > 1, Just hout)
+        else return (False, Nothing)
