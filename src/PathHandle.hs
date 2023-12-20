b4d064d6a0d7ea838db71e63a5dc45e306b32679
Desktop <kurosu.0122.playstation@gmail.com>
2023-12-20 09:09:48 +0900

    change a function

--- src/PathHandle.hs
+++ src/PathHandle.hs
@@ -1,16 +1,25 @@
-module PathHandle ( getPathRecursive ) where
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
-import System.Directory
-    ( doesDirectoryExist, getDirectoryContents )
-import           System.FilePath.Posix
+module PathHandle ( getPath ) where
 
-getPathRecursive :: FilePath -> IO [FilePath]
-getPathRecursive "" = return []
-getPathRecursive path = do
-    isDir <- doesDirectoryExist path
-    if isDir
-        then do
-            contents <- getDirectoryContents path
-            paths <- mapM (getPathRecursive . (path </>)) $ filter (`notElem` [".", ".."]) contents
-            return $ concat paths
-    else return [path]
+import           Control.Monad    (filterM)
+import           Data.Foldable    (fold)
+import           System.Directory (doesDirectoryExist, listDirectory)
+import           System.IO.Unsafe (unsafeInterleaveIO)
+import           System.FilePath
+
+getPath :: (FilePath -> IO Bool) -- ^ Filepath filter
+                    -> FilePath
+                    -> IO [FilePath]
+getPath p fp = do
+    all' <- listDirectory fp
+    all'' <- filterM p (mkRel <$> all')
+    dirs <- filterM doesDirectoryExist all''
+    case dirs of
+        [] -> pure all''
+        ds -> do
+            next <- unsafeInterleaveIO $ foldMapA (getPath p) ds
+            pure $ all'' ++ next
+    where
+        mkRel = (fp </>)
+        foldMapA = (fmap fold .) . traverse
