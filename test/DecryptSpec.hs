30d1e1308e5c94430c927bda685475fd72f27c54
Auros-NotePC <92504306+Auros-engineerVersion@users.noreply.github.com>
2024-01-07 13:16:18 +0900

    tweak a test

--- test/DecryptSpec.hs
+++ test/DecryptSpec.hs
@@ -1,6 +1,6 @@
 module DecryptSpec (spec) where
 
-import           Decrypt          (decrypt, isEncrypted, isPDF)
+import           Decrypt          (decrypt, checkFileInfo, isPDF)
 import           System.Directory (removeFile)
 import           Test.Hspec       (Spec, describe, it, shouldReturn)
 
@@ -21,18 +21,18 @@ spec = do
 
     describe "isEncrypted" $ do
         it "暗号化されていないpdfの場合" $ do
-            isEncrypted "test/test_data/normal.pdf" `shouldReturn` False
+            checkFileInfo "test/test_data/normal.pdf" "-Encryption" `shouldReturn` False
 
         it "暗号化されたpdfの場合" $ do
-            isEncrypted "test/test_data/encrypted.pdf" `shouldReturn` True
+            checkFileInfo "test/test_data/encrypted.pdf" "-Encryption" `shouldReturn` True
 
     describe "decrypt" $ do
         it "パスワードが間違っている場合" $ do
             decrypt "test/test_data/encrypted.pdf" "test/test_data/out.pdf" ["wrong"]
-            isEncrypted "test/test_data/encrypted.pdf" `shouldReturn` True
+            checkFileInfo "test/test_data/encrypted.pdf" "-Encryption" `shouldReturn` True
 
         it "パスワードが正しい場合" $ do
             let out_path = "test/test_data/out.pdf"
             decrypt "test/test_data/encrypted.pdf" out_path ["password"]
-            isEncrypted out_path `shouldReturn` False
+            checkFileInfo out_path "-Encryption" `shouldReturn` False
             removeFile out_path
